c6ee593caf83f81ffba2c20cfa73b55ee8af8ced
RosyPanday <pandayrosy14@gmail.com>
2026-09-10 22:44:15 +0545

    fix: overlay transition

--- tailPanel/src/components/layout/layout.tsx
+++ tailPanel/src/components/layout/layout.tsx
@@ -1,14 +1,14 @@
-import type { LayoutProps } from '../../types/layout.js';
-import Navbar from './navbar/navbar.js';
-import Sidebar from './sidebar/sidebar.js';
+import type { LayoutProps } from "../../types/layout.js";
+import Navbar from "./navbar/navbar.js";
+import Sidebar from "./sidebar/sidebar.js";
 
-function Layout({ isExpanded,toggleSidebar,children}:LayoutProps) {
-  
+function Layout({ isExpanded, toggleSidebar, children }: LayoutProps) {
   return (
     <>
-      {isExpanded && (
-        <div className="fixed inset-0 h-screen w-full z-50 bg-black/30 lg:hidden" onClick={toggleSidebar}></div>
-      )}
+      <div
+        className={`fixed inset-0 transition-all duration-1000 h-screen w-full z-50 ${isExpanded ? "bg-black/30 " : "bg-black/0 "} lg:hidden`}
+        onClick={toggleSidebar}
+      ></div>
       {/* sidebar */}
       <div
         className={`${isExpanded ? "fixed w-60 sm:w-70 z-100  bg-white" : "w-0 bg-white lg:w-23 lg:block"} lg:sticky lg:top-0 h-screen transition-all duration-500`}
@@ -16,15 +16,11 @@ function Layout({ isExpanded,toggleSidebar,children}:LayoutProps) {
         <Sidebar isExpanded={isExpanded} toggleSidebar={toggleSidebar} />
       </div>
       {/* navbar */}
-      <div
-        className="flex-1 shrink-0 flex flex-col"
-      >
-        <div className='sticky top-0 bg-white z-10'>
-              <Navbar toggleSidebar={toggleSidebar}/>
+      <div className="flex-1 shrink-0 flex flex-col">
+        <div className="sticky top-0 bg-white z-10">
+          <Navbar toggleSidebar={toggleSidebar} />
         </div>
-        <main className="bg-gray-100 min-h-screen flex-1">
-          {children}
-        </main>
+        <main className="bg-gray-100 min-h-screen flex-1">{children}</main>
       </div>
     </>
   );
