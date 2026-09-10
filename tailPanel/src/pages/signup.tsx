9bbc4fbb5828f14b8cc10952778ace94d50dbf9d
RosyPanday <pandayrosy14@gmail.com>
2026-09-10 21:17:57 +0545

    image height fix

--- tailPanel/src/pages/signup.tsx
+++ tailPanel/src/pages/signup.tsx
@@ -3,18 +3,18 @@ import signupSideImg from "../assets/images/side-image.png";
 
 function Signup() {
   return (
-    <div className="flex bg-gray-100 w-full h-screen p-5 gap-5">
+    <div className="flex bg-gray-100 w-full h-screen p-5 gap-1">
       {/* Form Section */}
       <div className="flex flex-1 h-full overflow-y-auto">
         <SignupContent />
       </div>
 
       {/* Image Section */}
-      <div className="hidden md:block md:w-1/2 h-full">
+      <div className="hidden md:block md:w-1/2 md:h-screen md pb-6 ">
         <img
           src={signupSideImg}
           alt="Signup background"
-          className="h-full w-full  rounded-2xl"
+          className="h-full w-full object-fill rounded-2xl"
         />
       </div>
     </div>
